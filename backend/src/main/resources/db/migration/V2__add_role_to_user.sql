9d5d447bc368d80136bdd208c40118a6cff4a323
iamspase <iamspase@protonmail.com>
2025-08-27 03:04:53 +0300

    Update .gitignore

--- backend/src/main/resources/db/migration/V2__add_role_to_user.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE users
-    ADD role SMALLINT;
-
-ALTER TABLE users
-    ALTER COLUMN role SET NOT NULL;
--- backend/src/main/resources/db/migration/V4__add_account_ref.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE users
-    ADD account_id BIGINT;
-
-ALTER TABLE users
-    ADD CONSTRAINT uc_users_account UNIQUE (account_id);
-
-ALTER TABLE users
-    ADD CONSTRAINT FK_USERS_ON_ACCOUNT FOREIGN KEY (account_id) REFERENCES accounts (id);
